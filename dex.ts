916e8ca1294364c7306f4af8f0e2754bdabc45dd
ryudai <bigdragon.06102001@gmail.com>
2024-05-01 02:20:02 +0900

    コンテナ数が0または1のときに対応

--- dex.ts
+++ dex.ts
@@ -7,10 +7,24 @@ if (args.length === 0) {
 
 const result = await $`docker ps`.text();
 const containers = result.split("\n").slice(1);
-const selectedContainer = await $.select({
-  message: "Select a container",
-  options: containers,
-});
+
+if (containers.length === 0) {
+  Deno.exit();
+}
+
+let selectedContainer = 0;
+if (containers.length == 1) {
+  const response = await $.confirm({
+    message: `${containers[selectedContainer]}\nExec into this container?`,
+    default: true,
+  });
+  if (!response) Deno.exit();
+} else {
+  selectedContainer = await $.select({
+    message: "Select a container",
+    options: containers,
+  });
+}
 const selectedContainerId = containers[selectedContainer].split(/\s+/)[0];
 
 await $`docker exec -it ${selectedContainerId} ${args}`.noThrow();
